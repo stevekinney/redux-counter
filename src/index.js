f9cfab34feeef5ae0bed9276d006b90b2a5b9092
Steve Kinney <hello@stevekinney.net>
2019-05-29 11:25:35 -0500

    Get ready for decrement exercise

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
 import './styles.scss';
+
+const initialState = {
+  count: 0,
+};
+
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
+    const { count, increment } = this.props;
+    console.log({ count, increment });
+
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
+          <button onClick={increment}>Increment</button>
           <button>Decrement</button>
           <button>Reset</button>
         </section>
@@ -18,4 +46,26 @@ class Counter extends Component {
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = state => {
+  return state;
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment() {
+      dispatch(incrementValue());
+    },
+  };
+};
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
